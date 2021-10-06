248ddbd61ccd18aea10ac6df48888ec9307d67d8
Daniel Lima <danieldeveloper1@gmail.com>
2021-10-06 11:24:18 -0300

    create App.js and import in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
-    <div>React App</div>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );

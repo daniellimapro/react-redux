56e8dd4e54374dec35e68f4b0d971e33e7f77f34
Daniel Lima <danieldeveloper1@gmail.com>
2021-10-06 11:52:36 -0300

    import store using combineReducers

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,9 +1,10 @@
 import { useSelector } from "react-redux";
 
 const Catalog = () => {
-  const catalog = useSelector((state) => state);
+  const cart = useSelector((state) => state);
+  console.log(cart);
 
-  return <h1>{catalog.name}</h1>;
+  return <h1>Redux</h1>;
 };
 
 export default Catalog;

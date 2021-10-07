5d4e2ce7f143878773b442ef095713f855648963
Daniel Lima <danieldeveloper1@gmail.com>
2021-10-07 09:52:35 -0300

    change state using actions creators

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,8 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import { useDispatch } from "react-redux";
 import api from "../services/api";
+import { addProductToCart } from "../store/modules/cart/actions";
 
 const Catalog = () => {
   const [catalog, setCatalog] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     api.get("products").then((response) => {
@@ -10,13 +13,22 @@ const Catalog = () => {
     });
   }, []);
 
+  const handleAddProductToCart = useCallback(
+    (product) => {
+      dispatch(addProductToCart(product));
+    },
+    [dispatch]
+  );
+
   return (
     <>
       <h1>Catalog</h1>
       {catalog.map((product) => (
         <p key={product.id}>
           {product.title}, <b>{product.price}</b>{" "}
-          <button type="button">Comprar</button>
+          <button type="button" onClick={() => handleAddProductToCart(product)}>
+            Comprar
+          </button>
         </p>
       ))}
     </>
